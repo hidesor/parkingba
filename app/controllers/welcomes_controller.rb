c9515d8e43ae45cb7ba4f288c22967f77a2d2c2b
BenWu <hidesor@yahoo.com.tw>
2016-09-13 22:16:05 +0800

    首頁用id排序

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,7 +1,7 @@
 class WelcomesController < ApplicationController
   layout 'welcome'
   def index
-    @parks = Park.all
+    @parks = Park.all.order(:id)
     p '-----userx-----'
     p params[:userx]
     #  @projects = Project.search(params[:search])
