0a77b56eb26784fc2b6e187441738d1e6c96f167
BenWu <hidesor@yahoo.com.tw>
2016-09-24 09:47:53 +0800

    debug

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -1,12 +1,17 @@
 class WelcomesController < ApplicationController
   layout 'welcome'
   def index
-    @parks = Park.all.order(:id)
-    if params[:search].present?  
+    @parks = Park.all.order(:id
+
+    if params[:search].present?
+      p '-----userx-----'
+      p params[:search]
         @parks = Park.near(params[:search].to_s, 2, :order => "distance")
+        p '-----userx-----'
+        p @parks.first
+
     end
     p '-----userx-----'
-    p params[:userx]
     #  @projects = Project.search(params[:search])
   end
 end
