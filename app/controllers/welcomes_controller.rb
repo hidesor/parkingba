4a5a988eee92ecd13c91935b6567b17c5f69aa8a
BenWu <hidesor@yahoo.com.tw>
2016-09-24 10:16:14 +0800

    debug

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -6,7 +6,7 @@ class WelcomesController < ApplicationController
     if params[:search].present?
       p '-----userx-----'
       p params[:search]
-        @parks = Park.near(params[:search].to_s, 2, :order => "distance")
+        @parks = Park.near(params[:search].to_s, 2)
         p '-----userx-----'
         p @parks.first
 
