68d828849b3d422a586ae4e0fecee552405d5820
kaichen <kaichen@acloudis.com>
2016-09-24 09:20:10 +0800

    修改查詢範圍

--- app/controllers/welcomes_controller.rb
+++ app/controllers/welcomes_controller.rb
@@ -3,7 +3,7 @@ class WelcomesController < ApplicationController
   def index
     @parks = Park.all.order(:id)
     if params[:search].present?  
-        @parks = Park.near(params[:search].to_s, 100, :order => "distance")
+        @parks = Park.near(params[:search].to_s, 2, :order => "distance")
     end
     p '-----userx-----'
     p params[:userx]
